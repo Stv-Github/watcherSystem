67869f4a379e8e2b032a613e3b1acfdc099380a7
Stv-Github <1210987736@qq.com>
2018-09-18 23:34:28 +0800

    11

--- src/components/pages/dashBoard.vue
+++ src/components/pages/dashBoard.vue
@@ -1,17 +1,17 @@
 <template>
     <div>
-        <div class="crumbs">
+        <!--<div class="crumbs">
             <el-breadcrumb separator="/">
                 <el-breadcrumb-item>首页</el-breadcrumb-item>
             </el-breadcrumb>
-        </div>
+        </div>-->
         <el-row class="">
             <el-col :span='8'>
                 <div class="el_col_body el_card is_hover_shadow">
                     <div class="el_col_user">
                         <img src="../../../static/images/img.jpg" alt="">
                         <div>
-                            <h2>admin</h2>
+                            <div>admin</div>
                             <p>超级管理员</p>
                         </div>
                     </div>
@@ -26,7 +26,35 @@
             </el-col>
             <el-col :span='16'>
                 <div class="el_col_news">
-                    <div>34534534</div>
+                    <el-row class="grid_cont_row">
+                        <el-col :span='8'>
+                            <div class="el-card is_hover_shadow el_col_newsItems">
+                                <i class="el-icon-edit grid_cont_icon1"></i>
+                                <div class="grid_cont_right">
+                                    <p class="grid_num grid_num1">1234</p>
+                                    <p>用户访问量</p>
+                                </div>
+                            </div>
+                        </el-col>
+                        <el-col :span='8'>
+                            <div class="el-card is_hover_shadow el_col_newsItems">
+                                <i class="el-icon-edit grid_cont_icon2"></i>
+                                <div class="grid_cont_right">
+                                    <p class="grid_num grid_num2">564</p>
+                                    <p>系统消息</p>
+                                </div>
+                            </div>
+                        </el-col>
+                        <el-col :span='8'>
+                            <div class="el-card is_hover_shadow el_col_newsItems">
+                                <i class="el-icon-edit grid_cont_icon3"></i>
+                                <div class="grid_cont_right">
+                                    <p class="grid_num grid_num3">5000</p>
+                                    <p>数量</p>
+                                </div>
+                            </div>
+                        </el-col>
+                    </el-row>
                     <div>34534534</div>
                 </div>
             </el-col>
@@ -54,6 +82,7 @@
         border-radius: 4px;
         color: #303133;
         background-color: #FFF;
+        overflow: hidden;
     }
     .is_hover_shadow:hover {
         box-shadow: 0 3px 5px 2px rgba(0, 0, 0, .1);
@@ -61,7 +90,7 @@
         transition: all .3s ease;
     }
     .el-col {
-        /* padding: 0 10px; */
+         /*padding: 0 10px; */
     }
     
     /* end 公用样式 */
@@ -87,6 +116,9 @@
     .el_col_user > div {
         margin-left: 50px;
     }
+    .el_col_user > div div{
+        font-size: 25px;
+    }
     .el_col_user > div p {
         color: #999;
     }
@@ -101,8 +133,56 @@
         margin-left: 70px;
     }
 
+
+
     .el_col_news {
+        /*padding: 0 10px;*/
+    }
+    .grid_cont_row, .grid_cont_row .el-col {
         padding: 0 10px;
+    }
+    .el_col_newsItems {
+        display: flex;
+        height: 100px;
+        align-items: center;
+    }
+    .el_col_newsItems i {
+        display: block;
+        width: 100px;
+        height: 100px;
+        text-align: center;
+        line-height: 100px;
+        color: #fff;
+        font-size: 30px;
+    }
+    .grid_cont_icon1 {
+        background-color: #2D8CF0;
+    }
+    .grid_cont_icon2 {
+        background-color: #64d572;
+    }
+    .grid_cont_icon3 {
+        background-color: #f25e43;
+    }
+    .grid_cont_right {
+        flex: 1;
+        text-align: center;
+    }
+    .grid_cont_right p:last-child {
+        color: #999;
+    }
+    .grid_cont_right .grid_num {
+        font-size: 30px;
+        font-weight: bold;
+    }
+    .grid_num1 {
+        color: #2d8cf0;
+    }
+    .grid_num2 {
+        color: #64d572;
+    }
+    .grid_num3 {
+        color: #f25e43;
     }
     
 
